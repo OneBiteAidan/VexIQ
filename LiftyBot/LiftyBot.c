df4b20a221c41082b0b42ca401c65ba1ff84aa89
Joe S <kenwood364@gmail.com>
2019-08-23 12:23:41 -0400

    Repair Lifty Bot
    See issue #1 For more details

--- LiftyBot/LiftyBot.c
+++ LiftyBot/LiftyBot.c
@@ -16,27 +16,30 @@ int joystick_thro_ch = 2;
 
 task main()
 {
-	while(true);
+	while(true)
 	{
 		thro = vexRT[joystick_steering_ch];
 		steering = vexRT[joystick_thro_ch];
 
-			drivetrain(thro + steering, thro - steering);
-			delay(1)
-
+		drivetrain(thro + steering, thro - steering);
+		delay(1);
+		{
+			if (vexRT[BtnLUp] == 1)
 			{
-				if (vexRT[BtnLUp==1]);
-				motor[motor2]=(30);
-				else motor[motor2]=(0);
-				if vexRT[BtnLDown==1];
-				motor[motor2]=(-30);
-				else motor[motor2]=(0);
+				motor[motor2] = (30);
 			}
-
+			else
+			{
+				motor[motor2]=(0);
+			}
+			if (vexRT[BtnLDown] == 1)
+			{
+				motor[motor2] = (-30);
+			}
+			else
+			{
+				motor[motor2]=(0);
+			}
 		}
-
-
-
-
-
 	}
+}
